236ac56638d76062c7212d3d0c40419d75f29f2e
adrianne <adrianne.ng7@gmail.com>
2019-03-23 16:16:32 -0400

    Merge branch 'master' of https://github.com/peiyaaao/emprint
    # Conflicts:
    #	.idea/misc.xml
    #	.idea/modules.xml

--- app/src/main/java/com/example/techtogether/emprint/SecondLogin.java
+++ app/src/main/java/com/example/techtogether/emprint/SecondLogin.java
@@ -1,7 +1,11 @@
 package com.example.techtogether.emprint;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class SecondLogin extends AppCompatActivity {
 
@@ -9,6 +13,24 @@ public class SecondLogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-    }
 
+        final ImageButton healthcareprovider = findViewById(R.id.provider);
+        healthcareprovider.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(this, HealthCareProvider.class);
+                startActivity(intent);
+            }
+        });
+
+        final ImageButton patient = findViewById(R.id.patient);
+        patient.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(this, Patient.class);
+                startActivity(intent);
+            }
+        });
+
+    }
 }
