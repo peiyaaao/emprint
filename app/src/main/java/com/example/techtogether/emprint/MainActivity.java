c0ea0196652a0b1f82d2cd82a24c247e183249f9
shahzeel1 <35353418+shahzeel1@users.noreply.github.com>
2019-03-23 12:28:42 -0400

    First screen Front End

--- app/src/main/java/com/example/techtogether/emprint/MainActivity.java
+++ app/src/main/java/com/example/techtogether/emprint/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.techtogether.emprint;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final Button loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                openSecondLogin();
+            }
+        });
+
+
+
+        final Button signUpButton = findViewById(R.id.signUpButton);
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                openParaLogin();
+            }
+        });
+
+
+
+
+    }
+    public void openSecondLogin()
+    {
+        Intent intent = new Intent(this, SecondLogin.class);
+        startActivity(intent);
+    }
+
+    public void openParaLogin()
+    {
+        Intent intent2 = new Intent(this, ParaLogin.class);
+        startActivity(intent2);
     }
 }
