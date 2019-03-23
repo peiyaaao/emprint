58be939844bb07d4a0cbe76e6b7b7ab84a0e43c5
adrianne <adrianne.ng7@gmail.com>
2019-03-23 19:45:21 -0400

    Merge branch 'master' of https://github.com/peiyaaao/emprint
    # Conflicts:
    #	.idea/misc.xml
    #	.idea/modules.xml

--- app/src/main/java/com/example/techtogether/emprint/EmergencyContact.java
+++ app/src/main/java/com/example/techtogether/emprint/EmergencyContact.java
@@ -6,24 +6,23 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 
 public class EmergencyContact extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_emergency_contact);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_contactinfo);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
-
+    EditText emergencyname = (EditText) findViewById(R.id.name);
+    EditText emergencyphone = (EditText) findViewById(R.id.mobile);
+    EditText emergencyaddress = (EditText) findViewById(R.id.address);
+    EditText emergencycity = (EditText) findViewById(R.id.city);
+    EditText emergencystate = (EditText) findViewById(R.id.state);
+    EditText emergencyzip = (EditText) findViewById(R.id.zip);
+    EditText emergencycountry = (EditText) findViewById(R.id.country);
+    EditText emergencyid = (EditText) findViewById(R.id.id);
+    EditText emergencybirthday = (EditText) findViewById(R.id.birthday);
 }
