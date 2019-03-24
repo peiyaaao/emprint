4f0674c760d99611b9789595994b1ef7d440f533
Peiyao Chen <p72chen@edu.uwaterloo.ca>
2019-03-24 00:43:30 -0400

    Clean up the UI

--- app/src/main/java/com/example/techtogether/emprint/PatientSignUp.java
+++ app/src/main/java/com/example/techtogether/emprint/PatientSignUp.java
@@ -1,16 +1,19 @@
 package com.example.techtogether.emprint;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 
-public class PatientSignUp extends AppCompatActivity {
+public class PatientSignUp extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_patient_sign_up);
 
         this.setupButton();
--- app/src/main/java/com/example/techtogether/emprint/PatientSignUpForm.java
+++ app/src/main/java/com/example/techtogether/emprint/PatientSignUpForm.java
@@ -1,8 +1,8 @@
 package com.example.techtogether.emprint;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,7 +11,7 @@ import android.widget.EditText;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class PatientSignUpForm extends AppCompatActivity {
+public class PatientSignUpForm extends Activity {
     private EditText fullName;
     private EditText mobile;
     private EditText address;
